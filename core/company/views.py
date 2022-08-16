9db0e0fea306335cab25277adbd37801f321a078
Yonathan Soto <yotoelectronics@gmail.com>
2022-08-16 10:27:03 -0500

    11

--- core/company/views.py
+++ core/company/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import DetailView, ListView
 from .models import Company
 
 # Create your views here.
 
 
-class CompanyView(View):
+class CompanyView(DetailView):
     model = Company
-    template_name = 'templates/index.html'
+    template_name = 'templates/base.html'
