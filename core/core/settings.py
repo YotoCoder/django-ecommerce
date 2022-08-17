168142609c6bf10255aea0dfdd11d99797105825
Yonathan Soto <yotoelectronics@gmail.com>
2022-08-16 23:49:20 -0500

    st3

--- core/core/settings.py
+++ core/core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-i0daj_l_r%ilfw#&j^=h&=v%j6(#qqms5b58^=7#d7-vo5o3m-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
